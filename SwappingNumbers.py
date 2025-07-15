6592a36a49cbf8e476fea6f437d14482e20a4ac7
Sandhita Ghosh <sandhitag226@gmail.com>
2025-07-15 15:53:12 +0530

    Add files via upload

---
+++ SwappingNumbers.py
@@ -0,0 +1,11 @@
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+
+print("The numbers are: ",a, b)
+temp = a
+a = b
+b = temp
+
+print("after swapping: ")
+print("The numbers are: ",a, b)
+
