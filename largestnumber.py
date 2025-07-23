14d7b3ae7e4543853d19d699d5f4836ad97449e6
Sandhita Ghosh <sandhitag226@gmail.com>
2025-07-23 22:53:20 +0530

    Delete largestnumber.py

--- largestnumber.py
+++
@@ -1,7 +0,0 @@
-numbers = input("Enter numbers separated by spaces: ")
-
-numbers_list = [float(num) for num in numbers.split()]
-
-largest = max(numbers_list)
-
-print("The Largest Number is: ", largest)
